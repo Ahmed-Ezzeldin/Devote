ad8e5c1d149f3a8ca320f37fcfb5cbef7c920c40
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-07-01 20:19:02 +0200

    refactor core data

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Codemaker on 01/07/2022.
+//
+
+import Foundation
+
+
+// MARK: - ========================== Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - ========================== UI
+
+// MARK: - ========================== UX
